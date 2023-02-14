56cedf7e9e50c1fcf6103fa200cded31ef844598
Dominik Pavlicek <dominikpavlicek.business@gmail.com>
2023-02-14 22:10:50 +0100

    30_UpdatedDialogueGraphColours

--- Source/MounteaDialogueSystemEditor/Private/Helpers/MounteaDialogueGraphColors.h
+++ Source/MounteaDialogueSystemEditor/Private/Helpers/MounteaDialogueGraphColors.h
@@ -6,40 +6,40 @@ namespace MounteaDialogueGraphColors
 {
 	namespace NodeBody
 	{
-		const FLinearColor Default(0.1f, 0.1f, 0.1f);
-		const FLinearColor Root(0.5f, 0.5f, 0.5f, 0.1f);
-		const FLinearColor Error(1.0f, 0.0f, 0.0f);
+		constexpr FLinearColor Default(0.1f, 0.1f, 0.1f);
+		constexpr FLinearColor Root(0.5f, 0.5f, 0.5f, 0.1f);
+		constexpr FLinearColor Error(1.0f, 0.0f, 0.0f);
 	}
 
 	namespace NodeBorder
 	{
-		const FLinearColor Inactive(0.08f, 0.08f, 0.08f);
-		const FLinearColor Root(0.2f, 0.2f, 0.2f, 0.2f);
-		const FLinearColor Selected(1.00f, 0.08f, 0.08f);
-		const FLinearColor ActiveDebugging(1.0f, 1.0f, 0.0f);
-		const FLinearColor InactiveDebugging(0.4f, 0.4f, 0.0f);
-		const FLinearColor HighlightAbortRange0(0.0f, 0.22f, 0.4f);
-		const FLinearColor HighlightAbortRange1(0.0f, 0.4f, 0.22f);
-		const FLinearColor Disconnected(0.f, 0.f, 0.f);
-		const FLinearColor BrokenWithParent(1.f, 0.f, 1.f);
-		const FLinearColor QuickFind(0.f, 0.8f, 0.f);
+		constexpr FLinearColor Inactive(0.08f, 0.08f, 0.08f);
+		constexpr FLinearColor Root(0.2f, 0.2f, 0.2f, 0.2f);
+		constexpr FLinearColor Selected(1.00f, 0.08f, 0.08f);
+		constexpr FLinearColor ActiveDebugging(1.0f, 1.0f, 0.0f);
+		constexpr FLinearColor InactiveDebugging(0.4f, 0.4f, 0.0f);
+		constexpr FLinearColor HighlightAbortRange0(0.0f, 0.22f, 0.4f);
+		constexpr FLinearColor HighlightAbortRange1(0.0f, 0.4f, 0.22f);
+		constexpr FLinearColor Disconnected(0.f, 0.f, 0.f);
+		constexpr FLinearColor BrokenWithParent(1.f, 0.f, 1.f);
+		constexpr FLinearColor QuickFind(0.f, 0.8f, 0.f);
 	}
 
 	namespace Pin
 	{
-		const FLinearColor Diff(0.9f, 0.2f, 0.15f);
-		const FLinearColor Hover(1.0f, 0.7f, 0.0f);
-		const FLinearColor Default(0.02f, 0.02f, 0.02f);
-		const FLinearColor SingleNode(0.02f, 0.02f, 0.02f);
+		constexpr FLinearColor Diff(0.9f, 0.2f, 0.15f);
+		constexpr FLinearColor Hover(1.0f, 0.7f, 0.0f);
+		constexpr FLinearColor Default(0.02f, 0.02f, 0.02f);
+		constexpr FLinearColor SingleNode(0.02f, 0.02f, 0.02f);
 	}
 	
 	namespace Connection
 	{
-		const FLinearColor Default(1.0f, 1.0f, 1.0f);
+		constexpr FLinearColor Default(1.0f, 1.0f, 1.0f);
 	}
 
 	namespace Action
 	{
-		const FLinearColor DragMarker(1.0f, 1.0f, 0.2f);
+		constexpr FLinearColor DragMarker(1.0f, 1.0f, 0.2f);
 	}
 }
