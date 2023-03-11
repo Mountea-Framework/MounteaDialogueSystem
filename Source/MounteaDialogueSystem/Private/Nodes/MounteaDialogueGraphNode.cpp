fe5f8856563e0fa352e6afcf2481e493e9df17b4
Dominik Pavlicek <dominikpavlicek.business@gmail.com>
2023-03-11 11:39:09 +0100

    151_RequestDecoratorExample

--- Source/MounteaDialogueSystem/Private/Nodes/MounteaDialogueGraphNode.cpp
+++ Source/MounteaDialogueSystem/Private/Nodes/MounteaDialogueGraphNode.cpp
@@ -275,8 +275,6 @@ void UMounteaDialogueGraphNode::OnPasted()
 
 FText UMounteaDialogueGraphNode::GetDefaultTooltipBody() const
 {
-	//For some reason this prints out warning that it cannot format Int to Text for some reason
-	
 	const FText InheritsValue = bInheritGraphDecorators ? LOCTEXT("True","Yes") : LOCTEXT("False","No");
 	const FText Inherits = FText::Format(LOCTEXT("UMounteaDialogueGraphNode_InheritsTooltip", "Inherits Graph Decorators: {0}"), InheritsValue);
 
