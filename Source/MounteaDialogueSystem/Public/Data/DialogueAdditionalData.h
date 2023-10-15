5bb345b99391c0696d5b7f9ef2bccde8bfabc856
Dominik Pavlicek <dominikpavlicek.business@gmail.com>
2023-10-15 12:41:56 +0200

    Added description to Additional Data

--- Source/MounteaDialogueSystem/Public/Data/DialogueAdditionalData.h
+++ Source/MounteaDialogueSystem/Public/Data/DialogueAdditionalData.h
@@ -8,6 +8,10 @@
 /**
  * Predefined list of Additional Data for Mountea Dialogue Rows.
  * Provides easy to use way to enhance Dialogues with extra details, like Animation to play etc.
+ *
+ * To make variable for this DataAsset, create a new Blueprint Class using `DialogueAdditionalData` as Parent class.
+ * Add variables to that new Blueprint Class.
+ * Then you can create this Dialogue Additional Data using the newly defined Blueprint class.
  */
 UCLASS( Blueprintable, meta=(UsesHierarchy=true), BlueprintType, EditInlineNew, ClassGroup="Mountea|Dialogue", AutoExpandCategories=("Mountea, Dialogue"), DisplayName="Dialogue Additional Data")
 class MOUNTEADIALOGUESYSTEM_API UDialogueAdditionalData : public UObject
