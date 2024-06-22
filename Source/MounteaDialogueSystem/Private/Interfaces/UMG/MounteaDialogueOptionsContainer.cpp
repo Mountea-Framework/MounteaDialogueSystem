0591607b81ee0b44e351b66b537a1cb11de77e0c
Dominik Morse <dominikpavlicek.business@gmail.com>
2024-06-22 10:16:04 +0200

    Fixing Build issues

--- Source/MounteaDialogueSystem/Private/Interfaces/UMG/MounteaDialogueOptionsContainer.cpp
+++
@@ -1,7 +0,0 @@
-﻿// All rights reserved Dominik Morse (Pavlicek) 2024.
-
-
-#include "Interfaces/UMG/MounteaDialogueOptionsContainerInterface.h"
-
-
-// Add default functionality here for any IMounteaDialogueOptionsContainerInterface functions that are not pure virtual.
