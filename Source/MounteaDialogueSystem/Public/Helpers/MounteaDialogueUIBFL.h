86130c9800edffc0aa36f604bab1be64053b4ade
Dominik Morse <dominikpavlicek.business@gmail.com>
2024-10-10 22:31:32 +0200

    5.4 fixes

--- Source/MounteaDialogueSystem/Public/Helpers/MounteaDialogueUIBFL.h
+++ Source/MounteaDialogueSystem/Public/Helpers/MounteaDialogueUIBFL.h
@@ -5,6 +5,7 @@
 #include "CoreMinimal.h"
 #include "Kismet/BlueprintFunctionLibrary.h"
 #include "Widgets/Layout/Anchors.h"
+#include "Layout/Margin.h"
 #include "MounteaDialogueUIBFL.generated.h"
 
 class UMounteaDialogueGraphNode;
@@ -14,6 +15,7 @@ struct FDialogueRowData;
 struct FDialogueOptionData;
 struct FDialogueRow;
 struct FWidgetDialogueRow;
+struct FAnchors;
 
 USTRUCT(BlueprintType)
 struct FWidgetAdditionParams
