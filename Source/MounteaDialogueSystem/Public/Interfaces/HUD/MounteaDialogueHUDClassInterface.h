f95552a1afc590c8d360f818d43cb142b07c5f33
Dominik Morse <dominikpavlicek.business@gmail.com>
2024-10-10 22:05:47 +0200

    5.3 Build fix

--- Source/MounteaDialogueSystem/Public/Interfaces/HUD/MounteaDialogueHUDClassInterface.h
+++ Source/MounteaDialogueSystem/Public/Interfaces/HUD/MounteaDialogueHUDClassInterface.h
@@ -5,7 +5,7 @@
 #include "CoreMinimal.h"
 #include "UObject/Interface.h"
 #include "Widgets/Layout/Anchors.h"
-#include "SlateCore/Public/Layout/Margin.h"
+#include "Layout/Margin.h"
 #include "MounteaDialogueHUDClassInterface.generated.h"
 
 
