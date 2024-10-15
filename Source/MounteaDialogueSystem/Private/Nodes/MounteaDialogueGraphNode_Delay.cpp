286ec827345fe6d5b683802dc0dc641329997122
Dominik Morse <dominikpavlicek.business@gmail.com>
2024-10-15 23:37:31 +0200

    Delay Node

---
+++ Source/MounteaDialogueSystem/Private/Nodes/MounteaDialogueGraphNode_Delay.cpp
@@ -0,0 +1,51 @@
+﻿// All rights reserved Dominik Morse (Pavlicek) 2024
+
+
+#include "Nodes/MounteaDialogueGraphNode_Delay.h"
+
+#define LOCTEXT_NAMESPACE "MounteaDialogueGraphNode_DelayNode"
+
+UMounteaDialogueGraphNode_Delay::UMounteaDialogueGraphNode_Delay()
+	: DelayDuration(3.f)
+{
+#if WITH_EDITORONLY_DATA
+	NodeTitle = LOCTEXT("MounteaDialogueGraphNode_DelayNodeBaseTitle", "Delay Node");
+	NodeTypeName = LOCTEXT("MounteaDialogueGraphNode_DelayNodeBaseInternalTitle", "Delay Node");
+	
+	ContextMenuName = LOCTEXT("MounteaDialogueGraphNode_DelayNodeContextMenuName", "Delay Node");
+	
+	BackgroundColor = FLinearColor(FColor::FromHex("007ddc"));
+	NodeTooltipText = LOCTEXT("MounteaDialogueGraphNode_DelayTooltip", "* This Node does nothing but waits until its Timer expires\n* After expiration following node is selected automatically");
+#endif
+	
+	bAutoStarts = true;
+}
+
+void UMounteaDialogueGraphNode_Delay::ProcessNode_Implementation(const TScriptInterface<IMounteaDialogueManagerInterface>& Manager)
+{
+	Super::ProcessNode_Implementation(Manager);
+
+	
+}
+
+FText UMounteaDialogueGraphNode_Delay::GetDescription_Implementation() const
+{
+	return LOCTEXT("MounteaDialogueGraphNode_DelayNodeDescription", "Waiting Node with no extra logic.\nUseful for cutscenes or transitions.");
+}
+
+FText UMounteaDialogueGraphNode_Delay::GetNodeCategory_Implementation() const
+{
+	return LOCTEXT("MounteaDialogueGraphNode_DelayNodeCategory", "Utilities");
+}
+
+bool UMounteaDialogueGraphNode_Delay::ValidateNode(TArray<FText>& ValidationsMessages, const bool RichFormat)
+{
+	bool bSatisfied = Super::ValidateNode(ValidationsMessages, RichFormat);
+	bSatisfied = bSatisfied && DelayDuration > 0.0f;
+
+	// TODO: Implement return texts
+	
+	return bSatisfied;
+}
+
+#undef LOCTEXT_NAMESPACE
---
+++ Source/MounteaDialogueSystem/Public/Nodes/MounteaDialogueGraphNode_Delay.h
@@ -0,0 +1,40 @@
+﻿// All rights reserved Dominik Morse (Pavlicek) 2024
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "MounteaDialogueGraphNode.h"
+#include "MounteaDialogueGraphNode_Delay.generated.h"
+
+/**
+ * Mountea Dialogue Delay Node.
+ * 
+ * ...
+ */
+UCLASS(ClassGroup=("Mountea|Dialogue"), NotBlueprintable, DisplayName="Delay Node", meta=(ToolTip="Mountea Dialogue Tree: Delay Node"))
+class MOUNTEADIALOGUESYSTEM_API UMounteaDialogueGraphNode_Delay : public UMounteaDialogueGraphNode
+{
+	GENERATED_BODY()
+
+public:
+
+	UMounteaDialogueGraphNode_Delay();
+
+	virtual void ProcessNode_Implementation(const TScriptInterface<IMounteaDialogueManagerInterface>& Manager) override;
+
+protected:
+
+	UPROPERTY(SaveGame, Category="Mountea|Dialogue", EditAnywhere, BlueprintReadOnly, meta=(NoResetToDefault,Units = "s", UIMin = 0.01, ClampMin = 0.01))
+	float DelayDuration;
+
+private:
+
+	FTimerHandle TimerHandle_NodeDelay;
+
+#if WITH_EDITOR
+	virtual FText GetDescription_Implementation() const override;
+	virtual FText GetNodeCategory_Implementation() const override;
+	
+	virtual bool ValidateNode(TArray<FText>& ValidationsMessages, const bool RichFormat) override;
+#endif
+};
