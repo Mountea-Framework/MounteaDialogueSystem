531603b90e550189eba8717f337fb2c2bafb2f57
Dominik Pavlíček <dominikpavlicek.business@gmail.com>
2024-10-30 16:39:43 +0100

    Dialogue Manager Statics

--- Source/MounteaDialogueSystem/Private/Helpers/MounteaDialogueManagerStatics.cpp
+++ Source/MounteaDialogueSystem/Private/Helpers/MounteaDialogueManagerStatics.cpp
@@ -3,9 +3,84 @@
 
 #include "Helpers/MounteaDialogueManagerStatics.h"
 
+AActor* UMounteaDialogueManagerStatics::GetOwningActor(UObject* Target)
+{
+	return ExecuteIfImplements<AActor*>(Target, TEXT("GetOwningActor"), &IMounteaDialogueManagerInterface::Execute_GetOwningActor);
+}
+
+UMounteaDialogueContext* UMounteaDialogueManagerStatics::GetDialogueContext(UObject* Target)
+{
+	return ExecuteIfImplements<UMounteaDialogueContext*>(Target, TEXT("GetDialogueContext"), &IMounteaDialogueManagerInterface::Execute_GetDialogueContext);
+}
+
+EDialogueManagerState UMounteaDialogueManagerStatics::GetState(UObject* Target)
+{
+	return ExecuteIfImplements<EDialogueManagerState>(Target, TEXT("GetState"), &IMounteaDialogueManagerInterface::Execute_GetState);
+}
+
+void UMounteaDialogueManagerStatics::SetDialogueManagerState(UObject* Target, const EDialogueManagerState NewState)
+{
+	ExecuteIfImplements<void>(Target, TEXT("SetDialogueManagerState"), &IMounteaDialogueManagerInterface::Execute_SetDialogueManagerState, NewState);
+}
+
+EDialogueManagerState UMounteaDialogueManagerStatics::GetDefaultDialogueManagerState(UObject* Target)
+{
+	return ExecuteIfImplements<EDialogueManagerState>(Target, TEXT("GetDefaultDialogueManagerState"), &IMounteaDialogueManagerInterface::Execute_GetDefaultDialogueManagerState);
+}
+
+bool UMounteaDialogueManagerStatics::AddDialogueUIObject(UObject* Target, UObject* NewDialogueObject)
+{
+	return ExecuteIfImplements<bool>(Target, TEXT("AddDialogueUIObject"), &IMounteaDialogueManagerInterface::Execute_AddDialogueUIObject, NewDialogueObject);
+}
+
+bool UMounteaDialogueManagerStatics::AddDialogueUIObjects(UObject* Target,const TArray<UObject*>& NewDialogueObjects)
+{
+	return ExecuteIfImplements<bool>(Target, TEXT("AddDialogueUIObjects"), &IMounteaDialogueManagerInterface::Execute_AddDialogueUIObjects, NewDialogueObjects);
+}
+
+bool UMounteaDialogueManagerStatics::RemoveDialogueUIObject(UObject* Target, UObject* DialogueObjectToRemove)
+{
+	return ExecuteIfImplements<bool>(Target, TEXT("RemoveDialogueUIObject"), &IMounteaDialogueManagerInterface::Execute_RemoveDialogueUIObject, DialogueObjectToRemove);
+}
+
+bool UMounteaDialogueManagerStatics::RemoveDialogueUIObjects(UObject* Target, const TArray<UObject*>& DialogueObjectsToRemove)
+{
+	return ExecuteIfImplements<bool>(Target, TEXT("RemoveDialogueUIObjects"), &IMounteaDialogueManagerInterface::Execute_RemoveDialogueUIObjects, DialogueObjectsToRemove);
+}
+
+void UMounteaDialogueManagerStatics::SetDialogueUIObjects(UObject* Target, const TArray<UObject*>& NewDialogueObjects)
+{
+	ExecuteIfImplements<void>(Target, TEXT("SetDialogueUIObjects"), &IMounteaDialogueManagerInterface::Execute_SetDialogueUIObjects, NewDialogueObjects);
+}
+
+void UMounteaDialogueManagerStatics::ResetDialogueUIObjects(UObject* Target)
+{
+	ExecuteIfImplements<void>(Target, TEXT("ResetDialogueUIObjects"), &IMounteaDialogueManagerInterface::Execute_ResetDialogueUIObjects);
+}
+
+void UMounteaDialogueManagerStatics::SkipDialogueRow(UObject* Target)
+{
+	ExecuteIfImplements<void>(Target, TEXT("SkipDialogueRow"), &IMounteaDialogueManagerInterface::Execute_SkipDialogueRow);
+}
+
 void UMounteaDialogueManagerStatics::CallDialogueNodeSelected(UObject* Target, const FGuid& NodeGUID)
 {
 	ExecuteIfImplements<void>(Target, TEXT("CallDialogueNodeSelected"), &IMounteaDialogueManagerInterface::Execute_CallDialogueNodeSelected, NodeGUID);
+}
+
+void UMounteaDialogueManagerStatics::PrepareNode(UObject* Target)
+{
+	ExecuteIfImplements<void>(Target, TEXT("PrepareNode"), &IMounteaDialogueManagerInterface::Execute_PrepareNode);
+}
+
+void UMounteaDialogueManagerStatics::ProcessNode(UObject* Target)
+{
+	ExecuteIfImplements<void>(Target, TEXT("ProcessNode"), &IMounteaDialogueManagerInterface::Execute_ProcessNode);
+}
+
+void UMounteaDialogueManagerStatics::InitializeDialogue(UObject* Target, APlayerState* OwningPlayerState, const FDialogueParticipants& Participants)
+{
+	ExecuteIfImplements<void>(Target, TEXT("InitializeDialogue"), &IMounteaDialogueManagerInterface::Execute_InitializeDialogue, OwningPlayerState, Participants);
 }
 
 void UMounteaDialogueManagerStatics::StartDialogue(UObject* Target)
@@ -43,8 +118,37 @@ void UMounteaDialogueManagerStatics::SetDialogueWidget(UObject* Target, UUserWid
 	ExecuteIfImplements<void>(Target, TEXT("SetDialogueWidget"), &IMounteaDialogueManagerInterface::Execute_SetDialogueWidget, DialogueUIPtr);
 }
 
-void UMounteaDialogueManagerStatics::SetDialogueManagerState(UObject* Target, const EDialogueManagerState NewState)
+UUserWidget* UMounteaDialogueManagerStatics::GetDialogueWidget(UObject* Target)
 {
-	ExecuteIfImplements<void>(Target, TEXT("SetDialogueManagerState"), &IMounteaDialogueManagerInterface::Execute_SetDialogueManagerState, NewState);
+	return ExecuteIfImplements<UUserWidget*>(Target, TEXT("GetDialogueWidget"), &IMounteaDialogueManagerInterface::Execute_GetDialogueWidget);
 }
 
+TSubclassOf<UUserWidget> UMounteaDialogueManagerStatics::GetDialogueWidgetClass(UObject* Target)
+{
+	return ExecuteIfImplements<TSubclassOf<UUserWidget>>(Target, TEXT("GetDialogueWidgetClass"), &IMounteaDialogueManagerInterface::Execute_GetDialogueWidgetClass);
+}
+
+int32 UMounteaDialogueManagerStatics::GetDialogueWidgetZOrder(UObject* Target)
+{
+	return ExecuteIfImplements<int32>(Target, TEXT("GetDialogueWidgetZOrder"), &IMounteaDialogueManagerInterface::Execute_GetDialogueWidgetZOrder);
+}
+
+void UMounteaDialogueManagerStatics::SetDialogueWidgetZOrder(UObject* Target, const int32 NewZOrder)
+{
+	ExecuteIfImplements<void>(Target, TEXT("SetDialogueWidgetZOrder"), &IMounteaDialogueManagerInterface::Execute_SetDialogueWidgetZOrder, NewZOrder);
+}
+
+void UMounteaDialogueManagerStatics::StartExecuteDialogueRow(UObject* Target)
+{
+	ExecuteIfImplements<void>(Target, TEXT("StartExecuteDialogueRow"), &IMounteaDialogueManagerInterface::Execute_StartExecuteDialogueRow);
+}
+
+void UMounteaDialogueManagerStatics::FinishedExecuteDialogueRow(UObject* Target)
+{
+	ExecuteIfImplements<void>(Target, TEXT("FinishedExecuteDialogueRow"), &IMounteaDialogueManagerInterface::Execute_FinishedExecuteDialogueRow);
+}
+
+void UMounteaDialogueManagerStatics::TriggerNextDialogueRow(UObject* Target)
+{
+	ExecuteIfImplements<void>(Target, TEXT("TriggerNextDialogueRow"), &IMounteaDialogueManagerInterface::Execute_TriggerNextDialogueRow);
+}
--- Source/MounteaDialogueSystem/Public/Helpers/MounteaDialogueManagerStatics.h
+++ Source/MounteaDialogueSystem/Public/Helpers/MounteaDialogueManagerStatics.h
@@ -20,6 +20,8 @@ class MOUNTEADIALOGUESYSTEM_API UMounteaDialogueManagerStatics : public UBluepri
 	GENERATED_BODY()
 
 public:
+	
+	// --- Template functions ------------------------------
 
 	template<typename ReturnType, typename Func, typename... Args>
 	static ReturnType ExecuteIfImplements(UObject* Target, const TCHAR* FunctionName, Func Function, Args&&... args)
@@ -50,15 +52,154 @@ public:
 
 public:
 
+	// --- Actor functions ------------------------------
+
+	/**
+	 * Returns the owning actor for this Dialogue Manager Component.
+	 *
+	 * @param Target	Dialogue Manager object. Must implement `IMounteaDialogueManager` interface.
+	 * @return The owning actor for this Dialogue Manager Component.
+	 */
+	UFUNCTION(BlueprintCallable, BlueprintPure, Category="Mountea|Dialogue|Manager", meta=(CustomTag="MounteaK2Getter"))
+	static AActor* GetOwningActor(UObject* Target);
+
+	/**
+	 * Retrieves current Dialogue Context.
+	 * 
+	 * ❗ Could be null
+	 * @param Target	Dialogue Manager object. Must implement `IMounteaDialogueManager` interface.
+	 * @return DialogueContext	Dialogue Context is transient data holder for current dialogue instance.
+	 */
+	UFUNCTION(BlueprintCallable, BlueprintPure, Category="Mountea|Dialogue|Manager", DisplayName="GetDialogueContext", meta=(CustomTag="MounteaK2Getter"))
+	static UMounteaDialogueContext* GetDialogueContext(UObject* Target);
+
+	/**
+	 * Retrieves current Dialogue Manager State.
+	 * State defines whether Manager can start/close dialogue or not.
+	 *
+	 * @param Target	Dialogue Manager object. Must implement `IMounteaDialogueManager` interface.
+	 * @return ManagerState	Manager state value
+	 */
+	UFUNCTION(BlueprintCallable, BlueprintPure, Category="Mountea|Dialogue|Manager", meta=(CustomTag="MounteaK2Getter"))
+	static EDialogueManagerState GetState(UObject* Target);
+
+	/**
+	 * Sets new Dialogue Manager State.
+	 *
+	 * @param Target	Dialogue Manager object. Must implement `IMounteaDialogueManager` interface.
+	 * @param NewState	Manager State to be set as Manager State
+	 */
+	UFUNCTION(BlueprintCallable, Category="Mountea|Dialogue|Manager", meta=(CustomTag="MounteaK2Getter"))
+	static void SetDialogueManagerState(UObject* Target, const EDialogueManagerState NewState);
+
+	/**
+	 * Retrieves current Default Dialogue Manager State.
+	 * Default Dialogue Manager State sets Dialogue Manager state upon BeginPlay and is used as fallback once Dialogue ends.
+	 *
+	 * @param Target	Dialogue Manager object. Must implement `IMounteaDialogueManager` interface.
+	 * @return ManagerState	Default Manager state value
+	 */
+	UFUNCTION(BlueprintCallable, BlueprintPure, Category="Mountea|Dialogue|Manager", meta=(CustomTag="MounteaK2Getter"))
+	static EDialogueManagerState GetDefaultDialogueManagerState(UObject* Target);
+
+	/**
+	 * Adds a single dialogue UI object to the manager.
+	 *
+	 * @param Target	Dialogue Manager object. Must implement `IMounteaDialogueManager` interface.
+	 * @param NewDialogueObject The object to add.
+	 * @return True if the object was successfully added, false otherwise.
+	 */
+	UFUNCTION(BlueprintCallable, Category="Mountea|Dialogue|Manager", meta=(CustomTag="MounteaK2Setter"))
+	static bool AddDialogueUIObject(UObject* Target, UObject* NewDialogueObject);
+
+	/**
+	 * Adds an array of dialogue UI objects to the manager.
+	 *
+	 * @param Target	Dialogue Manager object. Must implement `IMounteaDialogueManager` interface.
+	 * @param NewDialogueObjects Array of objects to add.
+	 * @return Number of objects successfully added.
+	 */
+	UFUNCTION(BlueprintCallable, Category="Mountea|Dialogue|Manager", meta=(CustomTag="MounteaK2Setter"))
+	static bool AddDialogueUIObjects(UObject* Target,const TArray<UObject*>& NewDialogueObjects);
+
+	/**
+	 * Removes a single dialogue UI object from the manager.
+	 *
+	 * @param Target	Dialogue Manager object. Must implement `IMounteaDialogueManager` interface.
+	 * @param DialogueObjectToRemove The object to remove.
+	 * @return True if the object was successfully removed, false otherwise.
+	 */
+	UFUNCTION(BlueprintCallable, Category="Mountea|Dialogue|Manager", meta=(CustomTag="MounteaK2Setter"))
+	static bool RemoveDialogueUIObject(UObject* Target, UObject* DialogueObjectToRemove);
+
+	/**
+	 * Removes multiple dialogue UI objects from the manager.
+	 *
+	 * @param Target	Dialogue Manager object. Must implement `IMounteaDialogueManager` interface.
+	 * @param DialogueObjectsToRemove Array of objects to remove.
+	 * @return Number of objects successfully removed.
+	 */
+	UFUNCTION(BlueprintCallable, Category="Mountea|Dialogue|Manager", meta=(CustomTag="MounteaK2Setter"))
+	static bool RemoveDialogueUIObjects(UObject* Target, const TArray<UObject*>& DialogueObjectsToRemove);
+
+	/**
+	 * Sets the entire array of dialogue UI objects, replacing any existing objects.
+	 *
+	 * @param Target	Dialogue Manager object. Must implement `IMounteaDialogueManager` interface.
+	 * @param NewDialogueObjects The new array of dialogue UI objects.
+	 */
+	UFUNCTION(BlueprintCallable, Category="Mountea|Dialogue|Manager", meta=(CustomTag="MounteaK2Setter"))
+	static void SetDialogueUIObjects(UObject* Target, const TArray<UObject* >& NewDialogueObjects);
+
+	/**
+	 * Resets the dialogue manager, removing all dialogue UI objects.
+	 *
+	 *  @param Target	Dialogue Manager object. Must implement `IMounteaDialogueManager` interface.
+	 */
+	UFUNCTION(BlueprintCallable, Category="Mountea|Dialogue|Manager", meta=(CustomTag="MounteaK2Setter"))
+	static void ResetDialogueUIObjects(UObject* Target);
+	
+	// --- Node functions ------------------------------
+
 	/**
 	 * Notifies the Dialogue  that a node has been selected.
 	 * 
 	 * @param Target	Dialogue Manager object. Must implement `IMounteaDialogueManager` interface.
 	 * @param NodeGUID	The GUID of the selected node.
 	 */
-	UFUNCTION(BlueprintCallable, Category="Mountea|Dialogue|Manager", meta=(Keywords="select,chosen,option"))
+	UFUNCTION(BlueprintCallable, Category="Mountea|Dialogue|Manager", meta=(Keywords="select,chosen,option"), meta=(CustomTag="MounteaK2Setter"))
 	static void CallDialogueNodeSelected(UObject* Target, const FGuid& NodeGUID);
 
+	/**
+	 * Prepares the node for execution.
+	 * Asks Active Node to 'PreProcessNode' and then to 'ProcessNode'.
+	 * In this preparation stage, Nodes are asked to process all Decorators.
+	 *
+	 * @param Target	Dialogue Manager object. Must implement `IMounteaDialogueManager` interface.
+	 */
+	UFUNCTION(BlueprintCallable, Category="Mountea|Dialogue|Manager", meta=(CustomTag="MounteaK2Setter"))
+	static void PrepareNode(UObject* Target);
+
+	/**
+	 * Calls to the Node to Process it.
+	 *
+	 * @param Target	Dialogue Manager object. Must implement `IMounteaDialogueManager` interface.
+	 */
+	UFUNCTION(BlueprintCallable, Category="Mountea|Dialogue|Manager", meta=(CustomTag="MounteaK2Setter"))
+	static void ProcessNode(UObject* Target);
+
+	// --- Dialogue functions ------------------------------
+
+	/**
+	 * Initializes the dialogue with the provided player state and participants. Provides the Manager and World to Nodes and Dialogue Graph.
+	 *
+	 * @param Target	Dialogue Manager object. Must implement `IMounteaDialogueManager` interface.
+	 * @param OwningPlayerState		The player state that owns this dialogue instance.
+	 * @param Participants					A structure containing all the participants involved in the dialogue.
+	 */
+	UFUNCTION(BlueprintCallable, Category="Mountea|Dialogue|Manager", meta=(Keywords="start,begin,initialize,initiate"), meta=(CustomTag="MounteaK2Setter"))
+	static void InitializeDialogue(UObject* Target, APlayerState* OwningPlayerState, const FDialogueParticipants& Participants);
+	
 	/**
 	 * Starts the Dialogue if possible.
 	 *
@@ -74,6 +215,8 @@ public:
 	 */
 	UFUNCTION(BlueprintCallable, Category="Mountea|Dialogue|Manager", meta=(Keywords="exit,close"), meta=(CustomTag="MounteaK2Setter"))
 	static void CloseDialogue(UObject* Target);
+
+	// --- Widget functions ------------------------------
 	
 	/**
 	 * Tries to Invoke Dialogue UI.
@@ -133,12 +276,80 @@ public:
 	static void SetDialogueWidget(UObject* Target, UUserWidget* DialogueUIPtr);
 
 	/**
-	 * Sets new Dialogue Manager State.
+	 * Returns the widget used to display the current dialogue.
+	 * 
+	 * @param Target	The target UObject containing the dialogue widget to receive the command. Must implement MounteaDialogueManagerInterface.
+	 * @return The widget used to display the current dialogue.
+	 */
+	UFUNCTION(BlueprintCallable, BlueprintPure, Category="Mountea|Dialogue|Manager", meta=(Keywords="UI"), meta=(CustomTag="MounteaK2Getter"))
+	static UUserWidget* GetDialogueWidget(UObject* Target);
+
+	/**
+	 * Gets the widget class used to display Dialogue UI.
+	 * 
+	 * @return The widget class used to display Dialogue UI.
+	 */
+	UFUNCTION(BlueprintCallable, BlueprintPure, BlueprintPure, Category="Mountea|Dialogue|Manager", meta=(Keywords="UI, Widget"), meta=(CustomTag="MounteaK2Getter"))
+	static TSubclassOf<UUserWidget> GetDialogueWidgetClass(UObject* Target);
+
+	/**
+	 * Retrieves the Z-order of the dialogue widget.
+	 * The Z-order determines the rendering order of the widget, with higher values rendering on top of lower values.
 	 *
 	 * @param Target	Dialogue Manager object. Must implement `IMounteaDialogueManager` interface.
-	 * @param NewState	Manager State to be set as Manager State
+	 * @return The current Z-order value of the dialogue widget.
 	 */
-	UFUNCTION(BlueprintCallable, Category="Mountea|Dialogue|Manager", meta=(Keywords="state,status"), meta=(CustomTag="MounteaK2Setter"))
-	static void SetDialogueManagerState(UObject* Target, const EDialogueManagerState NewState);
+	UFUNCTION(BlueprintCallable, BlueprintPure, Category="Mountea|Dialogue|Manager", meta=(CustomTag="MounteaK2Getter"))
+	static int32 GetDialogueWidgetZOrder(UObject* Target);
+
+	/**
+	 * Sets a new Z-order for the dialogue widget.
+	 * The Z-order determines the rendering order of the widget, with higher values rendering on top of lower values.
+	 * ❗ Runtime changes are not allowed!
+	 *
+	 * @param Target	Dialogue Manager object. Must implement `IMounteaDialogueManager` interface.
+	 * @param NewZOrder The new Z-order value to be applied to the dialogue widget.
+	 */
+	UFUNCTION(BlueprintCallable, Category="Mountea|Dialogue|Manager", meta=(CustomTag="MounteaK2Setter"))
+	static void SetDialogueWidgetZOrder(UObject* Target, const int32 NewZOrder);
+
+	// --- Dialogue Row functions ------------------------------
+
+	/**
+	 * Starts Dialogue Row execution.
+	 * ❔ Dialogue Data contain Dialogue Data Rows, which are individual dialogue lines, which can be skipped.
+	 * ❔ Once all Dialogue Data Rows are finished, Dialogue Data is finished as well.
+	 *
+	 * @param Target	The target UObject containing the dialogue widget to receive the command. Must implement MounteaDialogueManagerInterface.
+	 */
+	UFUNCTION(BlueprintCallable, Category="Mountea|Dialogue|Manager", meta=(CustomTag="MounteaK2Setter"))
+	static void StartExecuteDialogueRow(UObject* Target);
+
+	/**
+	 * Function responsible for cleanup once Dialogue Row is finished.
+	 * ❔ Dialogue Data contain Dialogue Data Rows, which are individual dialogue lines, which can be skipped.
+	 * ❔ Once all Dialogue Data Rows are finished, Dialogue Data is finished as well.
+	 * 
+	 * @param Target	The target UObject containing the dialogue widget to receive the command. Must implement MounteaDialogueManagerInterface.
+	 */
+	UFUNCTION(BlueprintCallable, Category="Mountea|Dialogue|Manager", meta=(CustomTag="MounteaK2Setter"))
+	static void FinishedExecuteDialogueRow(UObject* Target);
+
+	/**
+	 * Requests next dialogue row.
+	 * Contains validation that current row must be 'ExecutionMode::AwaitInput'.
+	 *
+	 * @param Target	The target UObject containing the dialogue widget to receive the command. Must implement MounteaDialogueManagerInterface.
+	 */
+	UFUNCTION(BlueprintCallable, Category="Mountea|Dialogue|Manager", meta=(CustomTag="MounteaK2Setter"))
+	static void TriggerNextDialogueRow(UObject* Target);
+
+	/**
+	 * Skips the current dialogue row.
+	 *
+	 * @param Target	Dialogue Manager object. Must implement `IMounteaDialogueManager` interface.
+	 */
+	UFUNCTION(BlueprintCallable, Category="Mountea|Dialogue|Manager", meta=(CustomTag="MounteaK2Setter"))
+	static void SkipDialogueRow(UObject* Target);
 
 };
