658fab07006d9ff721b99539c3e19e64a5ae0475
Dominik Morse <dominikpavlicek.business@gmail.com>
2025-07-26 20:44:20 +0200

    Add quick create shortcut for Dialogue Nodes
    Introduces FMounteaDialogueNodeAssetAction and UMounteaDialogueNodeFactory to support custom asset actions and creation for Mountea Dialogue Graph Nodes. Registers the new asset action and adds icon resources for dialogue nodes in the editor style.

---
+++ Source/MounteaDialogueSystemEditor/Private/Factories/MounteaDialogueNodeFactory.h
@@ -0,0 +1,29 @@
+﻿// All rights reserved Dominik Morse 2024
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Factories/Factory.h"
+#include "MounteaDialogueNodeFactory.generated.h"
+
+class UMounteaDialogueGraphNode;
+
+/**
+ * 
+ */
+UCLASS()
+class MOUNTEADIALOGUESYSTEMEDITOR_API UMounteaDialogueNodeFactory : public UFactory
+{
+	GENERATED_BODY()
+
+	UMounteaDialogueNodeFactory();
+	
+public:
+	virtual UObject* FactoryCreateNew(UClass* Class, UObject* InParent, FName Name, EObjectFlags Flags, UObject* Context, FFeedbackContext* Warn) override;
+	virtual bool ConfigureProperties() override;
+
+private:
+	// Holds the template of the class we are building
+	UPROPERTY()
+	TSubclassOf<UMounteaDialogueGraphNode> ParentClass;
+};
