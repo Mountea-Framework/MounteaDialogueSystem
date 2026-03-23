a997ede79f95dc365541bf27fafeeed2b9cf4fa6
Dominik Morse <aenszguhelem@gmail.com>
2026-03-23 22:10:02 +0100

    Add: Write-in Dialogue Node

---
+++ Source/MounteaDialogueSystem/Private/Nodes/MounteaDialogueGraphNode_DialogueNode.cpp
@@ -0,0 +1,68 @@
+﻿// Copyright (C) 2025 Dominik (Pavlicek) Morse. All rights reserved.
+//
+// Developed for the Mountea Framework as a free tool. This solution is provided
+// for use and sharing without charge. Redistribution is allowed under the following conditions:
+//
+// - You may use this solution in commercial products, provided the product is not
+//   this solution itself (or unless significant modifications have been made to the solution).
+// - You may not resell or redistribute the original, unmodified solution.
+//
+// For more information, visit: https://mountea.tools
+
+#include "Nodes/MounteaDialogueGraphNode_DialogueNode.h"
+
+#define LOCTEXT_NAMESPACE "MounteaDialogueGraphNode_DelayNode"
+
+UMounteaDialogueGraphNode_DialogueNode::UMounteaDialogueGraphNode_DialogueNode()
+{
+	NodeTitle = LOCTEXT("MounteaDialogueGraphNode_DialogueNodeBaseTitle", "Dialogue Node");
+	NodeTypeName = LOCTEXT("MounteaDialogueGraphNode_DialogueNodeBaseInternalTitle", "Dialogue Node");
+#if WITH_EDITORONLY_DATA
+	ContextMenuName = LOCTEXT("MounteaDialogueGraphNode_DialogueNodeContextMenuName", "Editable Dialogue Node");
+	
+	EditorNodeColour = FLinearColor::FromSRGBColor(FColor::FromHex(TEXT("297373")));
+	EditorHeaderForegroundColour = FLinearColor::White;
+	NodeTooltipText = LOCTEXT("MounteaDialogueGraphNode_DialogueNodeTooltip", "* Editable Dialogue Node which allows direct input instead of using Data Table.");
+#endif
+	
+	bAutoStarts = false;
+	
+	AllowedInputClasses.Add(UMounteaDialogueGraphNode::StaticClass());
+
+	MaxChildrenNodes = -1;
+}
+
+bool UMounteaDialogueGraphNode_DialogueNode::SetSpeechData_Implementation(const FDialogueRow& NewSpeechData)
+{
+	if (NewSpeechData.IsNearlyEqual(Dialogue))
+		return false;
+	Dialogue = NewSpeechData;
+	return true;
+}
+
+FText UMounteaDialogueGraphNode_DialogueNode::GetNodeTitle_Implementation() const
+{
+	return LOCTEXT("MounteaDialogueGraphNode_DialogueNodeTitle", "Dialogue Speech Node");
+}
+
+#if WITH_EDITOR
+
+FText UMounteaDialogueGraphNode_DialogueNode::GetDescription_Implementation() const
+{
+	return LOCTEXT("MounteaDialogueGraphNode_DialogueNodeDescription", "Editable Dialogue Node which allows direct input instead of using Data Table.");
+}
+
+FText UMounteaDialogueGraphNode_DialogueNode::GetNodeCategory_Implementation() const
+{
+	return LOCTEXT("MounteaDialogueGraphNode_DialogueNodeCategory", "Dialogue");
+}
+
+bool UMounteaDialogueGraphNode_DialogueNode::ValidateNode(FDataValidationContext& Context, const bool RichFormat) const
+{
+	return Super::ValidateNode(Context, RichFormat);
+}
+
+#endif
+
+
+#undef LOCTEXT_NAMESPACE
---
+++ Source/MounteaDialogueSystem/Public/Nodes/MounteaDialogueGraphNode_DialogueNode.h
@@ -0,0 +1,70 @@
+﻿// Copyright (C) 2025 Dominik (Pavlicek) Morse. All rights reserved.
+//
+// Developed for the Mountea Framework as a free tool. This solution is provided
+// for use and sharing without charge. Redistribution is allowed under the following conditions:
+//
+// - You may use this solution in commercial products, provided the product is not
+//   this solution itself (or unless significant modifications have been made to the solution).
+// - You may not resell or redistribute the original, unmodified solution.
+//
+// For more information, visit: https://mountea.tools
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "MounteaDialogueGraphNode_DialogueNodeBase.h"
+#include "Data/MounteaDialogueGraphDataTypes.h"
+#include "MounteaDialogueGraphNode_DialogueNode.generated.h"
+
+/**
+ * 
+ */
+UCLASS(ClassGroup=("Mountea|Dialogue"), DisplayName="Dialogue Speech Node", 
+	meta=(ToolTip="Mountea Dialogue Tree: Dialogue Speech Node"))
+class MOUNTEADIALOGUESYSTEM_API
+	UMounteaDialogueGraphNode_DialogueNode : public UMounteaDialogueGraphNode, public IMounteaDialogueSpeechDataInterface
+{
+	GENERATED_BODY()
+	
+public:
+	
+	UMounteaDialogueGraphNode_DialogueNode();
+	
+	inline virtual FDialogueRow GetSpeechData_Implementation() const override
+	{ return Dialogue; };
+	virtual bool SetSpeechData_Implementation(const FDialogueRow& NewSpeechData) override;
+	
+protected:
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Dialogue",
+		meta=(NoResetToDefault),
+		meta=(ShowOnlyInnerProperties))
+	FDialogueRow Dialogue;
+	
+public:
+	
+#if WITH_EDITORONLY_DATA
+
+	/**
+	 * Shows read-only Texts with localization of selected Dialogue Row.
+	 */
+	UPROPERTY(Transient, VisibleAnywhere, Category="Base", 
+		meta=(MultiLine=true, ShowOnlyInnerProperties),
+		meta=(HiddenInGraph))
+	TArray<FText> Preview;
+
+	FSimpleDelegate PreviewsUpdated;
+
+#endif
+	
+public:
+	
+	virtual FText GetNodeTitle_Implementation() const override;
+	
+#if WITH_EDITOR
+	virtual FText GetDescription_Implementation() const override;
+	virtual FText GetNodeCategory_Implementation() const override;
+	
+	virtual bool ValidateNode(FDataValidationContext& Context, const bool RichFormat) const override;
+#endif
+};
