863f4f0f5b7e95dfc4359ca1d12c762585ee2cf7
Dominik Morse <aenszguhelem@gmail.com>
2026-04-03 22:08:07 +0200

    Cleanup: removed interface bodies

--- Source/MounteaDialogueSystem/Private/Interfaces/HUD/MounteaDialogueHUDClassInterface.cpp
+++
@@ -1,7 +0,0 @@
-﻿// All rights reserved Dominik Morse (Pavlicek) 2024
-
-
-#include "Interfaces/HUD/MounteaDialogueHUDClassInterface.h"
-
-
-// Add default functionality here for any IMounteaDialogueHUDClassInterface functions that are not pure virtual.
